e19a183e9746614338ced4524b6b2ae83af2f9a3
Kirill Kudzin <46342464+Annunakh@users.noreply.github.com>
2020-01-26 00:46:01 +0300

    Add testGetClassString

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
 
     @Test
-    public void testGetClassNumber() {
-        Assert.assertTrue("The number should be greater than 45",this.getClassNumber() > 45);
+    public void testGetClassString() {
+        Assert.assertTrue("The string does not contain 'hello' or 'Hello'",
+                this.getClassString().contains("Hello") ||
+                        this.getClassString().contains("hello"));
     }
 }
